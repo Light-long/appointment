bca9ff96c0da5a3d05e0f6937e7b6e5b97190aaf
lishilong <lsl01107559@alibaba-inc.com>
2021-08-08 01:11:47 +0800

    模拟医院管理系统（用来上传信息到挂号平台）

---
+++ hospital_manager/src/main/java/com/terminus/hospital/mapper/OrderInfoMapper.java
@@ -0,0 +1,11 @@
+package com.terminus.hospital.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.terminus.hospital.model.OrderInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+}
